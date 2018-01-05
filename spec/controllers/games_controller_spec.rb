a7258299b8b66acf5abb4ae29c8268deffbe6a48
avbelyshev <avbelishev@gmail.com>
2018-01-05 23:41:31 +0300

    games_controller_spec. uses fifty_fifty

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -150,22 +150,46 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:alert]).to be
     end
 
-    # тест на отработку "помощи зала"
-    it 'uses audience help' do
-      # сперва проверяем что в подсказках текущего вопроса пусто
-      expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
-      expect(game_w_questions.audience_help_used).to be_falsey
+    # Группа тестов на использование подсказок
+    context 'Use help' do
+      # тест на отработку "помощи зала"
+      it 'uses audience help' do
+        # сперва проверяем что в подсказках текущего вопроса пусто
+        expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
+        expect(game_w_questions.audience_help_used).to be_falsey
 
-      # фигачим запрос в контроллен с нужным типом
-      put :help, id: game_w_questions.id, help_type: :audience_help
-      game = assigns(:game)
+        # фигачим запрос в контроллен с нужным типом
+        put :help, id: game_w_questions.id, help_type: :audience_help
+        game = assigns(:game)
 
-      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
-      expect(game.finished?).to be_falsey
-      expect(game.audience_help_used).to be_truthy
-      expect(game.current_game_question.help_hash[:audience_help]).to be
-      expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
-      expect(response).to redirect_to(game_path(game))
+        # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+        expect(game.finished?).to be_falsey
+        expect(game.audience_help_used).to be_truthy
+        expect(game.current_game_question.help_hash[:audience_help]).to be
+        expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+        expect(response).to redirect_to(game_path(game))
+      end
+
+      # тест на отработку "50/50"
+      it 'uses fifty_fifty' do
+        # сперва проверяем что в подсказках текущего вопроса пусто
+        expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+        expect(game_w_questions.fifty_fifty_used).to be_falsey
+
+        # фигачим запрос в контроллен с нужным типом
+        put :help, id: game_w_questions.id, help_type: :fifty_fifty
+        game = assigns(:game)
+
+        # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+        expect(game.finished?).to be_falsey
+        expect(game.fifty_fifty_used).to be_truthy
+
+        ff = game.current_game_question.help_hash[:fifty_fifty]
+        expect(ff).to be
+        expect(ff).to include('b')
+        expect(ff.size).to eq 2
+        expect(response).to redirect_to(game_path(game))
+      end
     end
   end
 end
