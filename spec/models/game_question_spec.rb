83f690479b718da3c9f39b7160d357e4cc91bd53
avbelyshev <avbelishev@gmail.com>
2018-01-05 22:45:00 +0300

    game_question_spec. friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -57,34 +57,47 @@ RSpec.describe GameQuestion, type: :model do
 
   # Группа тестов на помощь игроку
   context 'user helpers' do
+    let(:help_hash) { game_question.help_hash }
+
     # проверяем работоспосбность "помощи зала"
     it 'correct audience_help' do
       # сначала убедимся, в подсказках пока нет нужного ключа
-      expect(game_question.help_hash).not_to include(:audience_help)
+      expect(help_hash).not_to include(:audience_help)
       # вызовем подсказку
       game_question.add_audience_help
 
       # проверим создание подсказки
-      expect(game_question.help_hash).to include(:audience_help)
+      expect(help_hash).to include(:audience_help)
 
       # мы не можем знать распределение, но может проверить хотя бы наличие нужных ключей
-      ah = game_question.help_hash[:audience_help]
+      ah = help_hash[:audience_help]
       expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
     end
 
     # проверяем работу 50/50
     it 'correct fifty_fifty' do
       # сначала убедимся, в подсказках пока нет нужного ключа
-      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      expect(help_hash).not_to include(:fifty_fifty)
       # вызовем подсказку
       game_question.add_fifty_fifty
 
       # проверим создание подсказки
-      expect(game_question.help_hash).to include(:fifty_fifty)
-      ff = game_question.help_hash[:fifty_fifty]
+      expect(help_hash).to include(:fifty_fifty)
 
+      ff = help_hash[:fifty_fifty]
       expect(ff).to include('b') # должен остаться правильный вариант
       expect(ff.size).to eq 2 # всего должно остаться 2 варианта
+    end
+
+    it 'correct friend_call' do
+      expect(help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(help_hash).to include(:friend_call)
+
+      fc = help_hash[:friend_call]
+      expect(fc).to include('считает, что это вариант')
     end
   end
 end
